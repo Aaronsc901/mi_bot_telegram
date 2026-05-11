6d08b02a04aae581556a3e6ef5d84f6aa387cb4c
Aaronsc901 <60344414+Aaronsc901@users.noreply.github.com>
2026-05-11 07:06:09 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import requests
 TOKEN = os.getenv("TOKEN")
 
 # URL RAW del archivo JSON en GitHub
-URL_DATOS = "https://raw.githubusercontent.com/TU_USUARIO/mi_bot_telegram/master/datos.json"
+URL_DATOS = "https://raw.githubusercontent.com/Aaronsc901/mi_bot_telegram/master/datos.json"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("Consultar número", callback_data="consulta")]]
