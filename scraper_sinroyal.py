17f48ad8e07cd3d8ff003424652b1e2dbd556249
Aaronsc901 <60344414+Aaronsc901@users.noreply.github.com>
2026-05-21 16:28:03 -0400

    Update scraper_sinroyal.py

--- scraper_sinroyal.py
+++ scraper_sinroyal.py
@@ -13,9 +13,9 @@ TOKEN = os.getenv("TOKENX")
 if not TOKEN:
     raise Exception("ERROR: La variable de entorno TOKENX no está definida.")
 
-URL_GUACHARO = "https://loteriadehoy.com/animalitos/guacharo-activo/"
-URL_GRANJITA = "https://loteriadehoy.com/animalitos/la-granjita/"
-URL_LOTTO = "https://loteriadehoy.com/animalitos/lotto-activo/"
+URL_GUACHARO = "https://loteriadehoy.com/resultados/animalitos/guacharo-activo/"
+URL_GRANJITA = "https://loteriadehoy.com/resultados/animalitos/la-granjita/" 
+URL_LOTTO = "https://loteriadehoy.com/resultados/animalitos/lotto-activo/"
 
 def scrape_loteria(url):
     response = requests.get(url, timeout=10)
