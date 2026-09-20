aadd937b890f476c310412afc068312118dc8216
Aaronsc901 <60344414+Aaronsc901@users.noreply.github.com>
2026-09-20 11:02:55 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -518,11 +518,12 @@ def main():
     app.add_handler(CommandHandler("reset", reset))
     app.add_handler(CommandHandler("simular", simular))
     app.add_handler(CommandHandler("multi", multi))
-
+    print("REGISTRANDO HANDLERS...")
     app.add_handler(CallbackQueryHandler(handle_callback, pattern="^consulta$"))
-    app.add_handler(CallbackQueryHandler(handle_multi, pattern="^multi_"))
+    app.add_handler(CallbackQueryHandler(handle_multi, pattern="^multi_.$"))
 
     cargar_modo_test()
+    print("INICIANDO POLLING...")
     app.run_polling()
 
 if __name__ == "__main__":
